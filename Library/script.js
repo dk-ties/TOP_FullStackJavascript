6748bc3a1124948c5a94b679687d81a8d5c6838b
dk-ties <dk.ties@gmail.com>
2023-05-08 21:08:57 +0200

    Working on hide/show newBookform

--- Library/script.js
+++ Library/script.js
@@ -9,3 +9,13 @@ function getBook(title, author, pages) {
 const lordOfTheRings = new getBook("Lord of the rings", "J.R.R Tolkien", 295);
 
 console.log(lordOfTheRings.info);
+function showNewBookForm() {
+  showBookContainerDom.setAttribute("hidden", "");
+  newBookFormDom.removeAttribute("hidden");
+}
+
+const newBookFormDom = document.getElementById("newBookForm");
+const showBookContainerDom = document.getElementById("showBookContainer");
+
+const addNewBookClick = document.getElementById("addNewBook");
+addNewBookClick.addEventListener("click", showNewBookForm);
