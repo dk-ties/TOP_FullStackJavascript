bfeee67789375983f10fffd35d2407a77a49a5d8
dk-ties <dk.ties@gmail.com>
2023-05-10 07:55:12 +0200

    Setting up btn and input field in var

--- Library/script.js
+++ Library/script.js
@@ -19,3 +19,29 @@ const showBookContainerDom = document.getElementById("showBookContainer");
 
 const addNewBookClick = document.getElementById("addNewBook");
 addNewBookClick.addEventListener("click", showNewBookForm);
+
+// Setting up btn in form
+// Setting up sumit btn
+const btnSubmit = document.getElementById("btnSubmitForm");
+btnSubmit.addEventListener("click", funcSubmitForm);
+// Setting up form text filed for var
+const txtInputTitle = document.getElementById("newBookTitle");
+const txtInputAuthers = document.getElementById("newBookAuther");
+const txtInputPagenumbers = document.getElementById("newBookPages");
+const isReadInput = document.getElementById("newBookRead");
+
+function funcSubmitForm() {
+  console.log(txtInputTitle.value);
+  console.log(txtInputAuthers.value);
+  console.log(txtInputPagenumbers.value);
+  console.log(isReadInput.value);
+}
+// setting up reset btn
+const btnReset = document.getElementById("btnResetForm");
+btnReset.addEventListener("click", funcResetForm);
+
+function funcResetForm() {
+  console.log("hey babe, You reset me bitch!");
+}
+
+// Getting info from the form, on click
